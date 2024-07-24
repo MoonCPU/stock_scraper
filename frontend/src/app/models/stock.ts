bb4a55cb44e5d49edfeb72f5339f458929d0c3c8
MoonCPU <moonkang20@gmail.com>
2024-07-24 17:59:40 -0300

    adding cards to frontend

--- frontend/src/app/models/stock.ts
+++ frontend/src/app/models/stock.ts
@@ -1,51 +1,51 @@
-
 export interface Indicator {
-    valorAcao: number;
-    pl: string;
-    psr: string;
-    pvp: string;
-    dividendYield: string;
-    payout: string;
-    margemLiquida: string;
-    margemBruta: string;
-    margemEbit: string;
-    margemEbitda: string;
-    evEbitda: string;
-    evEbit: string;
-    pEbitda: string;
-    pEbit: string;
-    pAtivo: string;
-    pCapGiro: string;
-    pAtivoCircLiq: string;
-    vpa: string;
-    lpa: string;
-    giroAtivos: string;
-    roe: string;
-    roic: string;
-    roa: string;
-    dividaLiquidaPatrimonio: string;
-    dividaLiquidaEbitda: string;
-    dividaLiquidaEbit: string;
-    dividaBrutaPatrimonio: string;
-    patrimonioAtivos: string;
-    passivosAtivos: string;
-    liquidezCorrente: string;
-}
+    'Valor Ação': number;
+    'P/L': string;
+    'P/RECEITA (PSR)': string;
+    'P/VP': string;
+    'DIVIDEND YIELD - OIBR3': string;
+    'PAYOUT': string;
+    'MARGEM LÍQUIDA': string;
+    'MARGEM BRUTA': string;
+    'MARGEM EBIT': string;
+    'MARGEM EBITDA': string;
+    'EV/EBITDA': string;
+    'EV/EBIT': string;
+    'P/EBITDA': string;
+    'P/EBIT': string;
+    'P/ATIVO': string;
+    'P/CAP.GIRO': string;
+    'P/ATIVO CIRC LIQ': string;
+    'VPA': string;
+    'LPA': string;
+    'GIRO ATIVOS': string;
+    'ROE': string;
+    'ROIC': string;
+    'ROA': string;
+    'DÍVIDA LÍQUIDA / PATRIMÔNIO': string;
+    'DÍVIDA LÍQUIDA / EBITDA': string;
+    'DÍVIDA LÍQUIDA / EBIT': string;
+    'DÍVIDA BRUTA / PATRIMÔNIO': string;
+    'PATRIMÔNIO / ATIVOS': string;
+    'PASSIVOS / ATIVOS': string;
+    'LIQUIDEZ CORRENTE': string;
+    'CAGR RECEITAS 5 ANOS': string;
+  }
   
-export interface NetGrowth {
-    netRevenue: number;
-    cost: number;
-    grossProfit: number;
-    ebitda: number;
-    ebit: number;
-    tax: number;
-    netProfit: number;
-    netWorth: number;
+  export interface NetGrowth {
+    net_revenue: number;
+    cost?: number;
+    gross_profit?: number;
+    ebitda?: number;
+    ebit?: number;
+    tax?: number;
+    net_profit: number;
+    net_worth: number;
     year: string;
     quarter?: string;
-}
-
-export interface Stock {
+  }
+  
+  export interface Stock {
     indicators: Indicator;
-    netGrowth: NetGrowth[];
-}
+    net_growth: NetGrowth[];
+  }
--- frontend/src/app/stocks/stock-page/stock-page.component.ts
+++ frontend/src/app/stocks/stock-page/stock-page.component.ts
@@ -7,18 +7,69 @@ import { Stock } from 'src/app/models/stock';
   templateUrl: './stock-page.component.html',
   styleUrls: ['./stock-page.component.css']
 })
-
 export class StockPageComponent {
-  value = ''
-  stockData: Stock = {} as Stock;
+  value = '';
+  stockData: Stock[] = [];
   stockCode: string = '';
+  goodIndicators: { name: string, value: any }[] = [];
+  mediumIndicators: { name: string, value: any }[] = [];
+  badIndicators: { name: string, value: any }[] = [];
 
-  constructor(private stockService: StocksService){}
+  constructor(private stockService: StocksService) {}
 
   getStockData(stockCode: string): void {
     this.stockService.getStockData(stockCode).subscribe(data => {
-      this.stockData = data;
-      console.log('Received stock data:', this.stockData);
-    })
+      this.stockData.push(data);
+      this.categorizeIndicators(data.indicators);
+    });
+  }
+
+  categorizeIndicators(indicators: any): void {
+    for (let key in indicators) {
+      let value = indicators[key];
+
+      switch (key) {
+        case 'P/L':
+          this.categorizeValue(parseFloat(value), 10, 15, key, value);
+          break;
+        case 'P/VP':
+          this.categorizeValue(parseFloat(value), 1.5, 2.5, key, value);
+          break;
+        case 'LIQUIDEZ CORRENTE':
+          this.categorizeValue(parseFloat(value), 1, 2, key, value, false);
+          break;
+        case 'DIVIDEND YIELD - OIBR3':
+          if (value === '-' || parseFloat(value) < 2) {
+            this.badIndicators.push({ name: key, value: value });
+          } else if (parseFloat(value) < 5) {
+            this.mediumIndicators.push({ name: key, value: value });
+          } else if (parseFloat(value) <= 20) {
+            this.goodIndicators.push({ name: key, value: value });
+          }
+          break;
+        default:
+          break;
+      }
+    }
+  }
+
+  categorizeValue(value: number, lowerBound: number, upperBound: number, key: string, originalValue: any, ascending: boolean = true): void {
+    if (ascending) {
+      if (value < lowerBound) {
+        this.goodIndicators.push({ name: key, value: originalValue });
+      } else if (value < upperBound) {
+        this.mediumIndicators.push({ name: key, value: originalValue });
+      } else {
+        this.badIndicators.push({ name: key, value: originalValue });
+      }
+    } else {
+      if (value < lowerBound) {
+        this.badIndicators.push({ name: key, value: originalValue });
+      } else if (value < upperBound) {
+        this.mediumIndicators.push({ name: key, value: originalValue });
+      } else {
+        this.goodIndicators.push({ name: key, value: originalValue });
+      }
+    }
   }
 }
